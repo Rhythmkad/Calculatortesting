5510ae3b4389b11706a5eaf1afffa4c49aa11af0
Rhythmkad <91103424+Rhythmkad@users.noreply.github.com>
2022-02-21 10:45:43 -0800

    Add project files.

---
+++ Calculator.aspx
@@ -0,0 +1,192 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="Calculator.Calculator" %>
+
+
+
+
+
+<!DOCTYPE html>
+
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+
+    <title> Calculator</title>
+
+  
+    <link href="Calculator.css" rel="stylesheet" />
+
+</head>
+
+<body>
+
+    <form id="form1" runat="server">
+     <h1 style="margin-left:550px; color:darkturquoise">CALCULATER</h1>
+    <div class="calculator-holder">
+           
+    <table class="calculator" style="margin-left:500px;" id="calc">
+
+            <tr>
+
+                <td colspan="4" class="calc_td_result">
+
+                    <input type="text" readonly="readonly" runat="server" name="calc_result" id="calc_result" class="calc_result" style="height:42px;font-size:xx-large;" />
+
+                </td>
+
+            </tr>
+
+            <tr>
+
+                <td class="calc_td_btn">
+
+                        <asp:Button ID="ButtonCE" runat="server" CssClass="calc_btn" Text="CLR" OnClick="ButtonCE_Click" />
+
+                </td>
+
+                <td class="calc_td_btn">
+
+                          
+                    
+                </td>
+
+                <td >
+
+                        
+
+                </td>
+
+               
+
+            </tr>
+
+            <tr>
+
+                <td class="calc_td_btn">
+
+                        <asp:Button ID="Button7" runat="server" CssClass="calc_btn" Text="7" OnClick="Button7_Click" />
+
+                </td>
+
+                <td class="calc_td_btn">
+
+                        <asp:Button ID="Button8" runat="server" CssClass="calc_btn" Text="8" OnClick="Button8_Click" />
+
+                </td>
+
+                <td class="calc_td_btn">
+
+                        <asp:Button ID="Button9" runat="server" CssClass="calc_btn" Text="9" OnClick="Button9_Click" />
+
+                </td>
+
+                 <td class="calc_td_btn">
+
+                 
+                       <asp:Button ID="Button11" runat="server" CssClass="calc_btn" Text="+" OnClick="ButtonPlus_Click" />
+
+                </td>
+               
+
+            </tr>
+
+            <tr>
+
+                <td class="calc_td_btn">
+
+                        <asp:Button ID="Button4" runat="server" CssClass="calc_btn" Text="4" OnClick="Button4_Click" />
+
+                </td>
+
+                <td class="calc_td_btn">
+
+                        <asp:Button ID="Button5" runat="server" CssClass="calc_btn" Text="5" OnClick="Button5_Click" />
+
+                </td>
+
+                <td class="calc_td_btn">
+
+                        <asp:Button ID="Button6" runat="server" CssClass="calc_btn" Text="6" OnClick="Button6_Click" />
+
+                </td>
+                 <td class="calc_td_btn">
+
+                        <asp:Button ID="ButtonMinus" runat="server" CssClass="calc_btn" Text="-" OnClick="ButtonMinus_Click" />
+
+                </td>
+
+                
+
+            </tr>
+
+            <tr>
+
+                <td class="calc_td_btn">
+
+                        <asp:Button ID="Button1" runat="server" CssClass="calc_btn" Text="1" OnClick="Button1_Click" />
+
+                </td>
+
+                <td class="calc_td_btn">
+
+                        <asp:Button ID="Button2" runat="server" CssClass="calc_btn" Text="2" OnClick="Button2_Click" />
+
+                </td>
+
+                <td class="calc_td_btn">
+
+                        <asp:Button ID="Button3" runat="server" CssClass="calc_btn" Text="3" OnClick="Button3_Click"/>
+
+                </td>
+                <td class="calc_td_btn">
+
+                        <asp:Button ID="ButtonMultiply" runat="server" CssClass="calc_btn" Text="x" OnClick="ButtonMultiply_Click" />
+
+                </td>
+
+               
+
+            </tr>
+
+            <tr>
+
+                
+
+                <td class="calc_td_btn">
+
+                       <asp:Button ID="Button0" runat="server" CssClass="calc_btn" Text="0" OnClick="Button0_Click" />  
+
+                </td>
+                <td class="calc_td_btn" >
+
+                       <asp:Button ID="Button10" runat="server" CssClass="calc_btn" Text="&larr;" OnClick="ButtonReturn_Click" />
+
+                </td>
+                 <td class="calc_td_btn">
+
+                        <asp:Button ID="ButtonEquals" runat="server" CssClass="calc_btn" Text="=" OnClick="ButtonEquals_Click"/>
+
+                </td>
+
+                
+                 <td class="calc_td_btn">
+
+                        <asp:Button ID="ButtonDivide" runat="server" CssClass="calc_btn" Text="&divide;" OnClick="ButtonDivide_Click" />
+
+                </td>
+
+               
+               
+
+            </tr>
+
+        </table>
+         
+    </div>
+        
+    </form>
+
+</body>
+
+</html>
---
+++ Calculator.aspx.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        Calculate _Calculate;
+
+       
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _Calculate = new Calculate();
+        }
+       
+        // onClick Get Pass 0 Value 
+        protected void Button0_Click(object sender, EventArgs e)
+        {
+            calc_result.Value = calc_result.Value + "0";
+        }
+
+        // onClick Get Pass 1 Value 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            calc_result.Value = calc_result.Value + "1";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            calc_result.Value = calc_result.Value + "2";
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            calc_result.Value = calc_result.Value + "3";
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            calc_result.Value = calc_result.Value + "4";
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            calc_result.Value = calc_result.Value + "5";
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            calc_result.Value = calc_result.Value + "6";
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            calc_result.Value = calc_result.Value + "7";
+        }
+
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            calc_result.Value = calc_result.Value + "8";
+        }
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            calc_result.Value = calc_result.Value + "9";
+        }
+
+        // Addition work onClick
+        protected void ButtonPlus_Click(object sender, EventArgs e)
+        {
+            
+                ViewState["Value1"] = calc_result.Value;
+                ViewState["Operation"] = "Addition";
+                calc_result.Value = string.Empty;
+           
+        }
+
+        // Subtraction work onClick
+        protected void ButtonMinus_Click(object sender, EventArgs e)
+        {
+            
+                ViewState["Value1"] = calc_result.Value;
+                ViewState["Operation"] = "Subtraction";
+                calc_result.Value = string.Empty;
+           
+        }
+        // Multiplication work onClick
+        protected void ButtonMultiply_Click(object sender, EventArgs e)
+        {
+            
+                ViewState["Value1"] = calc_result.Value;
+                ViewState["Operation"] = "Multiplication";
+                calc_result.Value = string.Empty;
+           
+        }
+
+        // Division work onClick
+        protected void ButtonDivide_Click(object sender, EventArgs e)
+        {
+           
+                ViewState["Value1"] = calc_result.Value;
+                ViewState["Operation"] = "Division";
+                calc_result.Value = string.Empty;
+            
+        }
+
+
+        // Clear Textbox Input onClick
+        protected void ButtonCE_Click(object sender, EventArgs e)
+        {
+            calc_result.Value = "";
+            if ((string)ViewState["Operation"] != null)
+            {
+                ViewState["Operation"] = null;
+            }
+            else if ((string)ViewState["Value1"] != null)
+            {
+                ViewState["Value1"] = null;
+            }
+        }
+
+
+
+        // Clear Textbox last 1 digit Input onClick
+        protected void ButtonReturn_Click(object sender, EventArgs e)
+        {
+            
+                string CharactersInTextBox = calc_result.Value;
+                string FinalCharactersInTextBox = string.Empty;
+
+                for (int i = 0; i < CharactersInTextBox.Length - 1; i++)
+                {
+                    FinalCharactersInTextBox = FinalCharactersInTextBox + CharactersInTextBox[i];
+                }
+
+                calc_result.Value = FinalCharactersInTextBox;
+           
+        }
+
+        // Get Input Result onClick 
+        protected void ButtonEquals_Click(object sender, EventArgs e)
+        {
+
+            
+                ViewState["Value2"] = calc_result.Value;
+                calc_result.Value = string.Empty;
+
+                try
+                {
+                    if ((string)ViewState["Operation"] == "Addition")
+                    {
+                        calc_result.Value = _Calculate.Add(Convert.ToInt32(ViewState["Value1"]), Convert.ToInt32(ViewState["Value2"])).ToString();
+                    }
+                    else if ((string)ViewState["Operation"] == "Subtraction")
+                    {
+                        calc_result.Value = _Calculate.Subtract(Convert.ToInt32(ViewState["Value1"]), Convert.ToInt32(ViewState["Value2"])).ToString();
+                    }
+                    else if ((string)ViewState["Operation"] == "Multiplication")
+                    {
+                        calc_result.Value = _Calculate.Multiply(Convert.ToInt32(ViewState["Value1"]), Convert.ToInt32(ViewState["Value2"])).ToString();
+                    }
+                    else if ((string)ViewState["Operation"] == "Division")
+                    {
+                        calc_result.Value = _Calculate.Divide(Convert.ToInt32(ViewState["Value1"]), Convert.ToInt32(ViewState["Value2"])).ToString();
+                    }
+                    else if ((string)ViewState["Operation"] == "Percentage")
+                    {
+                        calc_result.Value = _Calculate.Percentage(Convert.ToInt32(ViewState["Value1"]), Convert.ToInt32(ViewState["Value2"])).ToString();
+                    }
+                    else Response.Write("<script>alert('No Operation was recorded.')</script>");
+                }
+                catch (FormatException)
+                {
+                    Response.Write("<script>alert('Bad Input Format.')</script>");
+                }
+        
+        }
+
+       
+
+    }
+
+}
